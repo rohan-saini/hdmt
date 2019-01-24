6e180ee7f1dbb836252055ed01d92c6dcd18c19d
Rohan Saini <rohansaini@gmail.com>
2019-01-24 12:47:59 -0500

    updates to front end and js

--- static/js/input.js
+++ static/js/input.js
@@ -1,28 +1,78 @@
 
+var uniquefilename;
+
 $(document).ready(function() {
-      $("#simBtn").click(function() {
+	$('[data-toggle="popover"]').popover()
 
-      var textToSave = "";
-      var elements = document.getElementsByTagName("input");
-      for (var i = 0; i < elements.length; i++) {
-          if(elements[i].value != "" && elements[i].id != "hidden") {
-              fieldToAdd = elements[i].value + ", ";
-              textToSave += fieldToAdd;
-          }
-      }
+	$(".pre-calculated").change(function() {
+		var maxAccum = $("#maxAccumulation").val();
+		var progStartMonths = $("#progStartMonths").val();
+		var progLastMonths = $("#progLastMonths").val();
+		var res = maxAccum/(progStartMonths - progLastMonths + 1);
+		$("#hedgeRate").val(res);
+	});
 
-      var datatosend = {
-          "textToSave": textToSave
-      }
-        //post request to call method that will run simulation
-        $.post( "/runSimulation", datatosend,
-                function(resp) {
-                    if(resp.message != "success") {
-                        alert("Error" + resp.data);
-                    }
-                    else {
-                        alert("Successfully saved inputs as " + resp.data);
-                    }
-                });
-      })
+	$("#simBtn").click(function() {
+		uniquefilename = new Date().getTime();
+		var textToSave = "";
+		var elements = document.getElementsByTagName("input");
+		for (var i = 0; i < elements.length; i++) {
+			if (elements[i].value != "" && elements[i].id != "hidden") {
+				fieldToAdd = elements[i].value + ", ";
+				if (elements[i].placeholder == "%") {
+                    fieldToAdd = fieldToAdd / 100;
+                }
+
+				textToSave += fieldToAdd;
+				textToSave += ",";
+				if (elements[i].name == "EndingMonth") {
+                    textToSave += ", ";
+                }
+			}
+		}
+
+		var datatosend = {
+			"textToSave": textToSave
+		}
+
+		// disabling button and changing text to "running"
+		$("#simBtn").attr("disabled", "true");
+        $("#simBtn").text("Running...");
+
+		//post request to call method that will run simulation
+		$.post( "/runSimulation", datatosend,
+	        function(resp) {
+				$("#simBtn").removeAttr("disabled");
+                $("#simBtn").text("Run Simulation");
+
+	            if(resp.message != "success") {
+	                alert("Error" + resp.data);
+	            }
+	            else {
+	                alert("Simulation started on the sever, click on output buttons to download the output files starting with " + uniquefilename + " prefix");
+	            }
+	        });
+	})
+
+	$("#outputBtn").click(function() {
+		$("#outputBtn").text("Getting...");
+		var datatosend = {
+			"filetype": "2",
+			"foldername": "output/" + uniquefilename + "/" + uniquefilename + "-Output.xlsx",
+			//"foldername": "output/" + uniquefilename + "/" + "Output.xlsx",
+			"localfilename": uniquefilename,
+			"downloadfileName": uniquefilename
+		}
+
+		//post request to call method that will run simulation
+		$.post( "/getOutput", datatosend,
+	        function(resp) {
+				if (resp) {
+					alert("got output");
+				}
+				else {
+					alert("Error getting file " + JSON.stringify(resp));
+				}
+	        });
+	})
 })
